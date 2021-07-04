af4c9ff01c63b3ae103097e950f807f66c1299c4
Thomas Jirout <jirout.thomas@gmail.com>
2021-07-04 20:01:26 +0200

    WIP: show abilities, types, stats and moves from pokemon entity

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,71 +1,49 @@
 <template>
   <div class="detail-view position-fixed" id="pokemon">
     <router-link to="/" style="position: sticky">Hide</router-link>
-    <h1>{{ $route.params.name }}</h1>
+    <h1>{{ $route.params.name }} (#{{ rawInfo.order }})</h1>
+    <h1 v-if="error == true">I've got no pokemon for you today</h1>
+    <article v-else>
+      Abilities:
+      <ul
+        v-bind:key="ability.ability.name"
+        v-for="ability in rawInfo.abilities"
+      >
+        <li>{{ ability.ability.name }}</li>
+      </ul>
 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis semper arcu
-    sit amet maximus feugiat. Cras maximus erat ac congue scelerisque. Praesent
-    ac ornare nisl, eget efficitur ante. Aenean eleifend est quis eros commodo
-    accumsan. Vestibulum ex est, viverra vitae tortor non, semper interdum odio.
-    Nam aliquam bibendum diam, nec vulputate elit blandit ut. Vestibulum ante
-    ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-    Aliquam sit amet quam nec velit imperdiet tincidunt in at nunc. Cras
-    vulputate quam metus, et hendrerit felis dapibus ac. Curabitur tristique
-    faucibus nibh id rhoncus. Sed blandit tortor lectus, sed auctor odio
-    venenatis vitae. Quisque nisi mi, feugiat sed tincidunt facilisis,
-    consectetur et sem. Vivamus vitae elit hendrerit, commodo risus commodo,
-    convallis elit. Etiam tristique venenatis facilisis. Phasellus lacinia ex
-    enim, sit amet tempor velit lobortis eget. Morbi neque lectus, ultrices ut
-    semper vel, tincidunt at enim. Lorem ipsum dolor sit amet, consectetur
-    adipiscing elit. Maecenas quis facilisis est. Phasellus interdum, enim quis
-    suscipit vehicula, diam diam condimentum est, bibendum egestas ipsum mi sit
-    amet erat. Nam at tincidunt turpis, et laoreet sapien. Suspendisse erat ex,
-    posuere id bibendum a, mattis sit amet ligula. Phasellus tempus hendrerit
-    orci. Cras hendrerit urna vitae viverra dignissim. Quisque consequat nisi
-    eget lacinia finibus. Praesent quis erat a odio tincidunt ornare. Curabitur
-    tempor pulvinar lacus, nec efficitur libero aliquam vitae. Curabitur
-    interdum volutpat ligula, vitae sodales nisi pharetra non. Vivamus fringilla
-    consequat metus, sit amet eleifend tellus. Nullam maximus metus vitae
-    vehicula ultrices. Nam sem diam, luctus ut bibendum fermentum, suscipit et
-    ligula. Curabitur tempor varius euismod. Etiam vulputate pulvinar suscipit.
-    Vivamus sollicitudin et nunc ac pellentesque. Nullam justo ex, feugiat a
-    pretium id, euismod nec metus. In tristique elit pellentesque nibh
-    efficitur, et euismod sapien rutrum. In condimentum arcu maximus tortor
-    egestas elementum. Suspendisse sit amet efficitur nisl. Sed consectetur
-    laoreet odio. Donec varius lectus lorem, ut pretium ex pellentesque vel.
-    Donec consequat ipsum a pulvinar scelerisque. Aenean hendrerit volutpat
-    interdum. Nullam mauris libero, pretium id est ac, finibus tempor erat. In
-    sollicitudin ultricies scelerisque. Phasellus luctus ultricies nunc non
-    vulputate. Sed ut ligula lacinia, rutrum diam pharetra, consectetur purus.
-    Nullam tempor dolor id elit tempor tempus. Ut vel augue quis elit imperdiet
-    gravida. Pellentesque ut purus nunc. Vestibulum tempus commodo magna et
-    rhoncus. Aenean dapibus vulputate ultrices. Aenean aliquam dictum elementum.
-    Sed vehicula pharetra tellus, eu placerat libero tincidunt quis. Nam
-    viverra, lacus quis laoreet molestie, nisl nisi laoreet tortor, nec
-    sollicitudin sapien magna at ante. Nam ut volutpat est, in placerat nulla.
-    Quisque hendrerit ipsum placerat, rhoncus nulla sit amet, interdum felis.
-    Aliquam sapien erat, condimentum ut velit at, gravida dictum sem. Proin
-    massa purus, consequat ac pulvinar sit amet, tincidunt et neque. Aenean et
-    eros purus. Nunc aliquam a enim pellentesque malesuada. Morbi ornare erat
-    eget ornare blandit. Orci varius natoque penatibus et magnis dis parturient
-    montes, nascetur ridiculus mus. Praesent tempor ultrices sollicitudin.
-    Suspendisse rhoncus, elit eget tristique vulputate, libero enim blandit
-    augue, ut mollis sapien sem vel ante. Maecenas tincidunt velit et eros
-    maximus, eget porttitor odio faucibus. Donec interdum arcu dolor, in mattis
-    massa iaculis feugiat. Nulla semper tortor ac sapien pulvinar fermentum.
-    Integer gravida ornare justo sit amet sodales. Etiam faucibus ligula est,
-    convallis posuere metus porta quis. Duis eu pulvinar magna. Donec nec est
-    sollicitudin, congue felis vitae, placerat neque. Nunc ultricies turpis quis
-    feugiat molestie. Etiam posuere, ante at eleifend commodo, turpis nisi
-    sagittis tortor, sit amet euismod nunc orci placerat tortor. Nulla sit amet
-    metus sed nisi facilisis varius et et magna. Nunc in tincidunt dolor.
-    Phasellus aliquet eros commodo ipsum egestas dignissim. Orci varius natoque
-    penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed
-    aliquam molestie lacus id commodo. Donec libero est, feugiat eget viverra
-    nec, interdum fermentum nisi. Maecenas consectetur, diam placerat dapibus
-    fringilla, dolor mi pulvinar urna, vitae condimentum leo orci ac ipsum.
-    Donec gravida posuere auctor. Curabitur scelerisque lectus est, at sodales
-    tellus efficitur a. Cras tempor purus eget bibendum porttitor.
+      Types:
+      <ul v-bind:key="type" v-for="type in rawInfo.types">
+        <li>{{ type.slot }} {{ type.type.name }}</li>
+      </ul>
+
+      Stats:
+      <ul v-bind:key="stat" v-for="stat in rawInfo.stats">
+        <li>
+          {{ stat.stat.name }}, base: {{ stat.base_stat }}, effor:
+          {{ stat.effort }}
+        </li>
+      </ul>
+
+      <a
+        class="btn btn-primary"
+        data-bs-toggle="collapse"
+        href="#collapseExample"
+        role="button"
+        aria-expanded="false"
+        aria-controls="collapseExample"
+      >
+        Show/Hide Moves
+      </a>
+      <ul
+        class="collapse"
+        id="collapseExample"
+        v-bind:key="move.move.name"
+        v-for="move in rawInfo.moves"
+      >
+        <li>{{ move.move.name }}</li>
+      </ul>
+    </article>
   </div>
 </template>
 
@@ -74,6 +52,93 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      info: "",
+      rawInfo: {
+        order: 42,
+        abilities: [
+          {
+            ability: {
+              name: "overgrow",
+              url: "https://pokeapi.co/api/v2/ability/65/",
+            },
+            is_hidden: false,
+            slot: 1,
+          },
+        ],
+        types: [
+          {
+            slot: 1,
+            type: { name: "bug", url: "https://pokeapi.co/api/v2/type/7/" },
+          },
+        ],
+        stats: [
+          {
+            base_stat: 45,
+            effort: 0,
+            stat: { name: "hp", url: "https://pokeapi.co/api/v2/stat/1/" },
+          },
+        ],
+        moves: [
+          {
+            move: {
+              name: "mega-punch",
+              url: "https://pokeapi.co/api/v2/move/5/",
+            },
+          },
+        ],
+      },
+      error: false,
+    };
+  },
+  computed: {
+    test() {
+      return "bla blu";
+    },
+  },
+  methods: {
+    fetch_pokemon(name) {
+      if (typeof name !== "string") {
+        return;
+      }
+
+      let baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+      let pokemonUrl = baseUrl + name;
+
+      fetch(pokemonUrl)
+        .then((response) => {
+          console.log(response);
+
+          if (response.status != 200) {
+            this.error = true;
+            throw "could not fetch pokemon";
+          } else {
+            this.error = false;
+          }
+
+          return response.json();
+        })
+        .then((json) => {
+          console.log("fetched pokemon", json);
+          this.info = JSON.stringify(json);
+          this.rawInfo = json;
+        });
+
+      let poke = document.querySelector("#pokemon");
+      poke.scrollTop = 0;
+    },
+  },
+  watch: {
+    $route(to, from) {
+      from;
+
+      this.fetch_pokemon(to.params.name);
+    },
+  },
+  mounted() {
+    this.fetch_pokemon(this.$route.params.name);
   },
 };
 </script>
